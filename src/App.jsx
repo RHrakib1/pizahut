883b16ebc08569bf8600ccc4e4ac3d6d9fad5c8a
rakib hasan <rakibhasanmd457@gmail.com>
2025-06-27 08:50:24 +0600

    add to the next [part]

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Banner from './Component/Banner/Banner'
 import Text from './Component/Text/Text'
 import Ordars from './Component/Ordars/Ordars'
 import Cooking from './Component/Cooking/Cooking'
+import CurrenlyCook from './Component/CurrenlyCook/CurrenlyCook'
 
 function App() {
 
   const [cooking, setcooking] = useState([])
-  const [preparing, setpreparing] = useState()
+  const [preparing, setpreparing] = useState([])
 
   const heandelCooking = (cook) => {
     setcooking([...cooking, cook])
@@ -17,7 +18,10 @@ function App() {
 
   const hendelPerparing = (id) => {
     const remaining = cooking.filter(item => item.recipe_id !== id);
+    const moveitem = cooking.find(item=>item.recipe_id == id)
+    
     setcooking(remaining);
+    setpreparing([...preparing,moveitem])
   }
 
   return (
@@ -28,7 +32,10 @@ function App() {
         <Text></Text>
         <div className='flex justify-between '>
           <Ordars heandelCooking={heandelCooking}></Ordars>
-          <Cooking hendelPerparing={hendelPerparing} cooking={cooking}></Cooking>
+          <div>
+            <Cooking hendelPerparing={hendelPerparing} cooking={cooking}></Cooking>
+            <CurrenlyCook preparing={preparing} ></CurrenlyCook>
+          </div>
         </div>
       </main>
     </>
---
+++ src/Component/CurrenlyCook/CurrenlyCook.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function CurrenlyCook({ preparing }) {
+    return (
+        <div>
+            <div className='w-96 shadow-2xl p-5'>
+                {/* <h1 className='text-3xl font-bold p-5'> Currenly cooking:{preparing.length} </h1> */}
+                <table className="table-auto w-full  border-gray-300">
+                    <thead className="bg-gray-200">
+                        <tr>
+                            <th className=" px-4 py-2">ID</th>
+                            <th className=" px-4 py-2">Name</th>
+                            <th className=" px-4 py-2">Calories</th>
+                            <th className=" px-4 py-2">Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        preparing.map((item, index) => (
+                            <tr key={index} className="hover:bg-gray-100">
+                                <td className=" px-4 py-2">{item.recipe_id}</td>
+                                <td className=" px-4 py-2">{item.recipe_name}</td>
+                                <td className=" px-4 py-2">{item.calories}</td>
+                                <td className=" px-4 py-2">{item.preparing_time} </td>
+                                
+                            </tr>
+                        ))
+                    }
+                </tbody>
+
+                </table>
+
+            </div>
+        </div>
+    )
+}
